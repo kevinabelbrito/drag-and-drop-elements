0ecb4a896eab97c4507463b3f564585f6396114b
Kevin Brito <kevinabelbrito@hotmail.com>
2024-06-11 17:36:12 -0400

    Example 2 touchable

--- example2/main.js
+++ example2/main.js
@@ -1,28 +1,58 @@
 let newX = 0, newY = 0, startX = 0, startY = 0;
 
-const card = document.getElementById('card')
+const card = document.getElementById('card');
 
-card.addEventListener('mousedown', mouseDown)
+card.addEventListener('mousedown', mouseDown);
+card.addEventListener('touchstart', touchStart, { passive: false });
 
-function mouseDown(e){
-    startX = e.clientX
-    startY = e.clientY
+function mouseDown(e) {
+    startX = e.clientX;
+    startY = e.clientY;
 
-    document.addEventListener('mousemove', mouseMove)
-    document.addEventListener('mouseup', mouseUp)
+    document.addEventListener('mousemove', mouseMove);
+    document.addEventListener('mouseup', mouseUp);
 }
 
-function mouseMove(e){
-    newX = startX - e.clientX 
-    newY = startY - e.clientY 
-  
-    startX = e.clientX
-    startY = e.clientY
+function mouseMove(e) {
+    newX = startX - e.clientX;
+    newY = startY - e.clientY;
 
-    card.style.top = (card.offsetTop - newY) + 'px'
-    card.style.left = (card.offsetLeft - newX) + 'px'
+    startX = e.clientX;
+    startY = e.clientY;
+
+    card.style.top = (card.offsetTop - newY) + 'px';
+    card.style.left = (card.offsetLeft - newX) + 'px';
 }
 
-function mouseUp(e){
-    document.removeEventListener('mousemove', mouseMove)
+function mouseUp(e) {
+    document.removeEventListener('mousemove', mouseMove);
+    document.removeEventListener('mouseup', mouseUp);
+}
+
+function touchStart(e) {
+    e.preventDefault();
+    const touch = e.touches[0];
+    startX = touch.clientX;
+    startY = touch.clientY;
+
+    document.addEventListener('touchmove', touchMove, { passive: false });
+    document.addEventListener('touchend', touchEnd, { passive: false });
+}
+
+function touchMove(e) {
+    e.preventDefault();
+    const touch = e.touches[0];
+    newX = startX - touch.clientX;
+    newY = startY - touch.clientY;
+
+    startX = touch.clientX;
+    startY = touch.clientY;
+
+    card.style.top = (card.offsetTop - newY) + 'px';
+    card.style.left = (card.offsetLeft - newX) + 'px';
+}
+
+function touchEnd(e) {
+    document.removeEventListener('touchmove', touchMove);
+    document.removeEventListener('touchend', touchEnd);
 }
